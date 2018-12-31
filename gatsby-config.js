2ed34835744ba1b4d9366214c4a8650a94b74f31
Gareth Fleming <gareth@joocy.com>
2018-12-31 11:54:27 +0000

    Starting to configure blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Joocy Blog`,
+    author: `Gareth Fleming`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `d3z`,
     },
   },
   plugins: [
